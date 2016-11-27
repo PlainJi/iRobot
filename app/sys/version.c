724e39e2926c3b761fd37ed0fc63f26c988de80a
Plain <plain_ji@163.com>
2016-11-27 21:28:53 +0800

    [CHG]change makefile

---
+++ app/sys/version.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAJOR_VERSION	1
+#define SUB_VERSION		0
+#define MINOR_VERSION	0
+
+void prt_soft_version(void)
+{
+	char timeTab[32];
+	time_t cur_time;
+	struct tm *p_tm;
+	cur_time = time(NULL);
+	p_tm = localtime(&cur_time);
+	sprintf((char*)timeTab, "%04d-%02d-%02d %02d:%02d:%02d", 
+		p_tm->tm_year+1900, p_tm->tm_mon+1, p_tm->tm_mday, p_tm->tm_hour, p_tm->tm_min, p_tm->tm_sec);
+	
+	printf("version: V%d.%d.%d build %s\n", MAJOR_VERSION, SUB_VERSION, MINOR_VERSION, timeTab);
+}
